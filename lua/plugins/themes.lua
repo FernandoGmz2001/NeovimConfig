b56d37032c4f00e8af8bf9efc2e6272a4d611973
FernandoGmz2001 <fernandogmz2001@gmail.com>
2025-09-04 14:06:35 -0500

    feat: changes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			require("onedarkpro").setup({
 				options = {
-					cursorline = true, -- Use cursorline highlighting?
+					-- cursorline = true, -- Use cursorline highlighting?
 					transparency = false, -- Use a transparent background?
 					terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
 					lualine_transparency = true, -- Center bar transparency?
